e1df77be4b6cf94957e04ceacab7468feab3d54a
Kani1902 <kanishka05.m@gmail.com>
2024-05-26 22:56:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const bakrid = new Date("2024-08-17T00:00:00").getTime();
+
+setInterval(function() {
+  const now = new Date().getTime();
+  
+  const difference = bakrid - now;
+
+  if (difference <= 0) {
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+    return; 
+  }
+  
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  
+  document.getElementById("days").innerText = days < 10 ? "0" + days : days;
+  document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").innerText = minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("seconds").innerText = seconds < 10 ? "0" + seconds : seconds;
+}, 1000); 
+
+
